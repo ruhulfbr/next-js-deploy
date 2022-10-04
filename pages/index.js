70dc39d0ad05b29feb7ecc59c97223f6ab740637
ruhulfbr <ruhul11bd@gmail.com>
2022-10-04 23:55:50 +0600

    this commit for vercel

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!, Try deploy to vercel</a>
         </h1>
 
+        <p className={styles.description}>This is Ruhul Amin</p>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
