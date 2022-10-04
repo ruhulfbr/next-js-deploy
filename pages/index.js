7835ca7e9d044b06078b7043867a0eeeead26f68
ruhulfbr <ruhul11bd@gmail.com>
2022-10-04 23:55:00 +0600

    initial Commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!, Try deploy in netify</a>
+          Welcome to <a href="https://nextjs.org">Next.js!, Try deploy to vercel</a>
         </h1>
 
         <p className={styles.description}>
